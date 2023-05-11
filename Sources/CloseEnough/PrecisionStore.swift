e45a0439fa77393229e0988dbbd1b4ead577b94a
Zev Eisenberg <zev@zeveisenberg.com>
2023-05-11 19:21:10 -0400

    Remove unused setter.

--- Sources/CloseEnough/PrecisionStore.swift
+++ Sources/CloseEnough/PrecisionStore.swift
@@ -4,20 +4,15 @@ struct PrecisionStore {
     private var storage: [ObjectIdentifier: Any] = [:]
 
     subscript<T: EquatableWithPrecision>(_ type: T.Type) -> T.Precision? {
-        get {
-            guard let anyPrecision = storage[ObjectIdentifier(type)] else {
-                XCTFail("Attempt to compare values of type \(T.self) without first registering precision for that type. Use 'withPrecisions(_:tests:)' and pass a type/precision pair for each type.")
-                return nil
-            }
-            guard let precision = anyPrecision as? T.Precision else {
-                XCTFail("Stored precision value \(anyPrecision) of type \(Swift.type(of: anyPrecision)) was not of expected type \(T.Precision.self), as specified by its \((any EquatableWithPrecision).self) conformance.")
-                return nil
-            }
-            return precision
+        guard let anyPrecision = storage[ObjectIdentifier(type)] else {
+            XCTFail("Attempt to compare values of type \(T.self) without first registering precision for that type. Use 'withPrecisions(_:tests:)' and pass a type/precision pair for each type.")
+            return nil
         }
-        set {
-            storage[ObjectIdentifier(type)] = newValue
+        guard let precision = anyPrecision as? T.Precision else {
+            XCTFail("Stored precision value \(anyPrecision) of type \(Swift.type(of: anyPrecision)) was not of expected type \(T.Precision.self), as specified by its \((any EquatableWithPrecision).self) conformance.")
+            return nil
         }
+        return precision
     }
 
     mutating func unregister<T: EquatableWithPrecision>(_ type: T.Type) {
